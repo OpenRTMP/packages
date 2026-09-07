9a66165de489c5ffa7bda372c7737b13bd1cddda
Alexander Wagner <github@alexanderwagnerdev.com>
2026-09-08 00:16:33 +0200

    Require signed Arch repository aliases

--- scripts/check-librtmp2-additional-package-completeness.sh
+++ scripts/check-librtmp2-additional-package-completeness.sh
@@ -63,9 +63,11 @@ check_arch() {
     check_file "$package" || missing=1
     check_file "$package.sig" || missing=1
     check_file "$base/openrtmp.db" || missing=1
+    check_file "$base/openrtmp.db.sig" || missing=1
     check_file "$base/openrtmp.db.tar.gz" || missing=1
     check_file "$base/openrtmp.db.tar.gz.sig" || missing=1
     check_file "$base/openrtmp.files" || missing=1
+    check_file "$base/openrtmp.files.sig" || missing=1
     check_file "$base/openrtmp.files.tar.gz" || missing=1
     check_file "$base/openrtmp.files.tar.gz.sig" || missing=1
 
